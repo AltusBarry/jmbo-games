d4d7a21e3f7634d51745e44ee4bc062924d14054
AltusBarry <AltusBarry@users.noreply.github.com>
2015-05-13 14:16:59 +0200

    Added more models

--- games/admin.py
+++ games/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
 from jmbo.admin import ModelBaseAdmin
-from games.models import TrivialContent
+from games.models import TrivialContent, Game, Reviewer
 
 
 admin.site.register(TrivialContent, ModelBaseAdmin)
+admin.site.register(Game, ModelBaseAdmin)
+admin.site.register(Reviewer, ModelBaseAdmin)
--- games/models.py
+++ games/models.py
@@ -1,6 +1,27 @@
+from django.db import models
 from jmbo.models import ModelBase
+from ckeditor.fields import RichTextField
 
 
 class TrivialContent(ModelBase):
     """We need one model so South migrations can be initiated."""
     pass
+
+
+class Game(ModelBase):
+    reviewer = models.ForeignKey("Reviewer")
+    """ 
+    review_text = RichTextField( blank=True, null=True, )
+    """
+    review_text = "TEXT"
+
+
+class Reviewer(ModelBase):
+    reviewer_name = models.CharField(max_length=100)
+
+    def __unicode__(self):
+        return self.reviewer_name
+
+    @property
+    def games(self):
+        return self.game_set.all().order_by("title")
