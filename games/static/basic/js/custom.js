103d90c543fd69bb27c366ccfd9d1b42e9507e33
hedleyroos <hedleyroos@gmail.com>
2015-05-22 16:29:50 +0200

    Toggle script

--- games/static/basic/js/custom.js
+++ games/static/basic/js/custom.js
@@ -1,9 +1,11 @@
 $(document).ready(function() {
-    $(document).on('click', "div.foundry-listing-vertical-thumbnail div.item-game a.toggle-characters-hide", function() {
+    $(document).on('click', "div.foundry-listing-vertical-thumbnail div.item-game a.toggle-characters", function() {
         console.log('toggle vis');
-        $(this).toggle();
-        $("div.foundry-listing-vertical-thumbnail div.item-game a.toggle-characters-show").toggle()
-        $('ul', $(this).closest('div.characters')).toggle();
+        var div = $('ul', $(this).closest('div.characters'));
+        div.toggle();
+        var a = $(this);
+        $('span', s).hide();
+        div.is(':visible') ? $('span.hide', a).show() : $('span.show', a).hide();
         return false;
     });
 });
